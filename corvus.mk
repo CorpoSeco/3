f83965b1c02ac5f06f001c5c4432252894f50e19
CorpoSeco <122288216+CorpoSeco@users.noreply.github.com>
2023-02-08 01:02:22 -0300

    Update corvus.mk

--- corvus.mk
+++ corvus.mk
@@ -4,7 +4,6 @@ $(call inherit-product, vendor/corvus/config/BoardConfigSoong.mk)
 $(call inherit-product, device/corvus/sepolicy/common/sepolicy.mk)
 -include vendor/corvus/build/core/config.mk
 
-
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 SELINUX_IGNORE_NEVERALLOWS := true
@@ -16,3 +15,4 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 CORVUS_MAINTAINER=TheAtt1la
 USE_GAPPS=true
+
